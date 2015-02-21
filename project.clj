324cc563c6102f9120fcc566eaaad3156a63c90c
Richard Möhn <richard.moehn@posteo.de>
2015-02-21 17:59:30 +0100

    Add what the reloaded template produced

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject shandor "0.1.0-SNAPSHOT"
+  :description "TODO"
+  :url "TODO"
+  :license {:name "TODO: Choose a license"
+            :url "http://choosealicense.com/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.n01se/clojure-jna "1.0.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
+                   :source-paths ["dev"]}})

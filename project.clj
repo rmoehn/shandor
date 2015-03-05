50fea1eaf50d10447b7864e60bd0a0ba1b91e660
Richard Möhn <richard.moehn@posteo.de>
2015-03-05 17:42:18 +0100

    Add JNA and Java path
    That I need JNA shouldn't be surprising. The Java path I put, because I
    thought I'd need some Java files to aid the C interop, but that turned
    out not to be the case. I'll leave it this way in case I need Java after
    all.

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
+
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  :javac-options ["-target" "1.7" "-source" "1.7"]
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.java.dev.jna/jna "4.1.0"]
                  [net.n01se/clojure-jna "1.0.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
                    :source-paths ["dev"]}})

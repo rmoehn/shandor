cff5e8087e3e44977d269d77a7d32673605bc7ae
Richard Möhn <richard.moehn@posteo.de>
2015-03-05 17:44:22 +0100

    Record my state of experimentation – tag extraction
    My user.clj looks quite messy right now, because I've been doing some
    trial and error in order to get the interop running. First major
    achievement: Write a function that takes a message struct and returns
    the tags belonging to the message.

--- dev/user.clj
+++ dev/user.clj
@@ -1,52 +1,67 @@
 (ns user
   "Tools for interactive development with the REPL. This file should
   not be included in a production build of the application."
-  (:require
-   [clojure.java.io :as io]
-   [clojure.java.javadoc :refer [javadoc]]
-   [clojure.pprint :refer [pprint]]
-   [clojure.reflect :refer [reflect]]
-   [clojure.repl :refer [apropos dir doc find-doc pst source]]
-   [clojure.set :as set]
-   [clojure.string :as str]
-   [clojure.test :as test]
-   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-   [shandor]))
+  (:require [clojure.java.io :as io]
+            [clojure.java.javadoc :refer [javadoc]]
+            [clojure.pprint :refer [pprint]]
+            [clojure.reflect :refer [reflect]]
+            [clojure.repl :refer [apropos dir doc find-doc pst source]]
+            [clojure.set :as set]
+            [clojure.string :as str]
+            [clojure.test :as test]
+            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+            [net.n01se.clojure-jna :as jna]
+            [shandor])
+  (:import [com.sun.jna.ptr PointerByReference]
+           [com.sun.jna Pointer]
+           ))
 
-(def system
-  "A Var containing an object representing the application under
-  development."
-  nil)
+(jna/to-ns nm notmuch [Integer notmuch_database_create,
+                       Integer notmuch_database_open,
+                       Integer notmuch_database_close,
+                       String notmuch_database_get_path,
+                       Integer notmuch_database_get_version,
+                       Integer notmuch_database_find_message,
+                       String notmuch_message_get_filename,
+                       Pointer notmuch_message_get_tags,
+                       Boolean notmuch_tags_valid,
+                       Void notmuch_tags_move_to_next,
+                       String notmuch_tags_get,
+                       Void notmuch_tags_destroy
+                       ])
 
-(defn init
-  "Creates and initializes the system under development in the Var
-  #'system."
-  []
-  ;; TODO
+(def mode {:read-only 0})
+
+(defn tags-from-msg [msg]
+  (let [tags-obj (nm/notmuch_message_get_tags msg)
+        res (loop [tags-vec []]
+              (if-some [tag (nm/notmuch_tags_get tags-obj)]
+                       (do (nm/notmuch_tags_move_to_next tags-obj)
+                         (recur (conj tags-vec tag)))
+                       tags-vec))]
+    (nm/notmuch_tags_destroy tags-obj)
+    res))
+
+(comment
+
+  (nm/notmuch_database_create "/home/erle/mail/kjellberg" db-pointer)
+
+  (def db-pointer (PointerByReference.))
+  (nm/notmuch_database_open "/home/erle/mail" (mode :read-only) db-pointer)
+  (def db (.getValue db-pointer))
+
+  (def msg-pointer (PointerByReference.))
+  (nm/notmuch_database_find_message db "20150305075606.GB2120@localhost.zedat.fu-berlin.de" msg-pointer)
+  (def msg (.getValue msg-pointer))
+
+  (nm/notmuch_message_get_filename msg)
+
+  (tags-from-msg msg)
+
+  (nm/notmuch_database_get_path (.getValue db-pointer))
+
+  (nm/notmuch_database_get_version (.getValue db-pointer))
+
+  (nm/notmuch_database_close (.getValue db-pointer))
+
   )
-
-(defn start
-  "Starts the system running, updates the Var #'system."
-  []
-  ;; TODO
-  )
-
-(defn stop
-  "Stops the system if it is currently running, updates the Var
-  #'system."
-  []
-  ;; TODO
-  )
-
-(defn go
-  "Initializes and starts the system running."
-  []
-  (init)
-  (start)
-  :ready)
-
-(defn reset
-  "Stops the system, reloads modified source files, and restarts it."
-  []
-  (stop)
-  (refresh :after 'user/go))
